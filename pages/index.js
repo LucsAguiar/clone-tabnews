74f35c17ebd51c02d851eabb1bf0fe7b43cf507b
LucsAguiar <160599519+LucsAguiar@users.noreply.github.com>
2024-02-29 12:06:11 +0000

    alteração de index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return <h1>Bomm dia. Hoje é quinta-feira, 29/02/2024. É o último dia do mês e a maré vai estar seca às 12:30h.
-    
-    </h1>
+    return <h1>Bomm dia! Eu sou Lucas Aguiar. Hoje é quinta-feira, 29/02/2024. É o último dia do mês e a maré vai estar seca às 12:26h.</h1>
 }
 
 export default Home;
